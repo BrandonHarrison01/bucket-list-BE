a0da7de4644fed988be6f2b9a61cc09c547ab712
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-25 21:09:10 -0600

    server is running

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors');
+const helmet = require('helmet')
+
+const server = express()
+
+// const usersRouter = require('./users/users-route')
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+// server.use('/api', usersRouter)
+
+server.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+module.exports = server;
